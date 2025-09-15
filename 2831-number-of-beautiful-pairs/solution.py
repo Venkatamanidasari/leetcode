c8d872e3c7af7f9f3b74e8843b5f5395d69ad4ee
Venkatamanidasari <venkatamanidasari0507@gmail.com>
2025-09-15 04:14:02 +0000

    Sync LeetCode submission Runtime - 174 ms (46.37%), Memory - 18.1 MB (100.00%)

---
+++ 2831-number-of-beautiful-pairs/solution.py
@@ -0,0 +1,18 @@
+from math import gcd
+from typing import List
+
+class Solution:
+    def countBeautifulPairs(self, nums: List[int]) -> int:
+        def first_digit(x: int) -> int:
+            while x >= 10:
+                x //= 10
+            return x
+
+        n = len(nums)
+        ans = 0
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if gcd(first_digit(nums[i]), nums[j] % 10) == 1:
+                    ans += 1
+        return ans
